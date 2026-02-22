0e7ce3bb4171981075b02bc93486b776f7af93e8
David M. <david.montero.isusi@gmail.com>
2026-02-22 01:19:15 +0100

    Refactor: Safely initialize task count, ensure correct PHP output for JavaScript, and improve code formatting.

--- modules/tasks/tasks.php
+++ modules/tasks/tasks.php
@@ -398,77 +398,77 @@ function toggle_users(id) {
 // else users would be able to arbitrarily run 'bad' functions
 if ($showEditCheckbox) {
 	?>
-	function checkAll(project_id) {
-		var f = eval('document.assFrm' + project_id);
-		var cFlag = f.master.checked ? false : true;
+		function checkAll(project_id) {
+			var f = eval('document.assFrm' + project_id);
+			var cFlag = f.master.checked ? false : true;
 	
-		for (var i=0;i< f.elements.length;i++) {
-			var e = f.elements[i];
-			// only if it's a checkbox.
-			if (e.type == "checkbox" && e.checked == cFlag && e.name != 'master') {
-				e.checked = !e.checked;
+			for (var i=0;i< f.elements.length;i++) {
+				var e = f.elements[i];
+				// only if it's a checkbox.
+				if (e.type == "checkbox" && e.checked == cFlag && e.name != 'master') {
+					e.checked = !e.checked;
+				}
 			}
+
 		}
 
-	}
-
-	function chAssignment(project_id, rmUser, del) {
-		var f = eval('document.assFrm' + project_id);
-		var fl = f.add_users.length-1;
-		var c = 0;
-		var a = 0;
+		function chAssignment(project_id, rmUser, del) {
+			var f = eval('document.assFrm' + project_id);
+			var fl = f.add_users.length-1;
+			var c = 0;
+			var a = 0;
 	
-		f.hassign.value = "";
-		f.htasks.value = "";
+			f.hassign.value = "";
+			f.htasks.value = "";
 	
-		// harvest all checked checkboxes (tasks to process)
-		for (var i=0;i< f.elements.length;i++) {
-			var e = f.elements[i];
-			// only if it's a checkbox.
-			if (e.type == "checkbox" && e.checked == true && e.name != 'master') {
-				c++;
-				f.htasks.value = f.htasks.value +", "+ e.value;
+			// harvest all checked checkboxes (tasks to process)
+			for (var i=0;i< f.elements.length;i++) {
+				var e = f.elements[i];
+				// only if it's a checkbox.
+				if (e.type == "checkbox" && e.checked == true && e.name != 'master') {
+					c++;
+					f.htasks.value = f.htasks.value +", "+ e.value;
+				}
 			}
-		}
 	
-		// harvest all selected possible User Assignees
-		for (fl; fl > -1; fl--) {
-			if (f.add_users.options[fl].selected) {
-				a++;
-				f.hassign.value = ", " + f.hassign.value +", "+ f.add_users.options[fl].value;
+			// harvest all selected possible User Assignees
+			for (fl; fl > -1; fl--) {
+				if (f.add_users.options[fl].selected) {
+					a++;
+					f.hassign.value = ", " + f.hassign.value +", "+ f.add_users.options[fl].value;
+				}
 			}
-		}
 	
-		if (del == true) {
-			if (c == 0) {
-				alert ('<?php echo $AppUI->_('Please select at least one Task!', UI_OUTPUT_JS); ?>');
-			} 
-			else if (a == 0 && rmUser == 1) {
-				alert ('<?php echo $AppUI->_('Please select at least one Assignee!', UI_OUTPUT_JS); ?>');
-			} 
-			else if (confirm('<?php echo $AppUI->_('Are you sure you want to unassign the User from Task(s)?', UI_OUTPUT_JS); ?>')) {
-				f.del.value = 1;
-				f.rm.value = rmUser;
-				f.project_id.value = project_id;
-				f.submit();
+			if (del == true) {
+				if (c == 0) {
+					alert ('<?php echo $AppUI->_('Please select at least one Task!', UI_OUTPUT_JS); ?>');
+				} 
+				else if (a == 0 && rmUser == 1) {
+					alert ('<?php echo $AppUI->_('Please select at least one Assignee!', UI_OUTPUT_JS); ?>');
+				} 
+				else if (confirm('<?php echo $AppUI->_('Are you sure you want to unassign the User from Task(s)?', UI_OUTPUT_JS); ?>')) {
+					f.del.value = 1;
+					f.rm.value = rmUser;
+					f.project_id.value = project_id;
+					f.submit();
+				}
 			}
-		}
-		else {
-		
-			if (c == 0) {
-				alert ('<?php echo $AppUI->_('Please select at least one Task!', UI_OUTPUT_JS); ?>');
-			} 
-			else if (a == 0) {
-				alert ('<?php echo $AppUI->_('Please select at least one Assignee!', UI_OUTPUT_JS); ?>');
-			} 
 			else {
-				f.rm.value = rmUser;
-				f.del.value = del;
-				f.project_id.value = project_id;
-				f.submit();
+		
+				if (c == 0) {
+					alert ('<?php echo $AppUI->_('Please select at least one Task!', UI_OUTPUT_JS); ?>');
+				} 
+				else if (a == 0) {
+					alert ('<?php echo $AppUI->_('Please select at least one Assignee!', UI_OUTPUT_JS); ?>');
+				} 
+				else {
+					f.rm.value = rmUser;
+					f.del.value = del;
+					f.project_id.value = project_id;
+					f.submit();
+				}
 			}
 		}
-	}
 <?php } ?>
 </script>
 
@@ -481,9 +481,9 @@ if ($showEditCheckbox) {
 				<?php if ($min_view) { ?>
 					<td>
 						<a href="<?php echo 'index.php' . $query_string . '&amp;open_task_all=1'; ?>"><?php
-						   echo $AppUI->_('Expand All'); ?></a> :
+							   echo $AppUI->_('Expand All'); ?></a> :
 						<a href="<?php echo 'index.php' . $query_string . '&amp;close_task_all=1'; ?>"><?php
-						   echo $AppUI->_('Collapse All'); ?></a>
+							   echo $AppUI->_('Collapse All'); ?></a>
 						&nbsp;&nbsp;
 					</td>
 				<?php }
@@ -532,7 +532,7 @@ if ($showEditCheckbox) {
 	reset($projects);
 	$actualProject = 0;
 	foreach ($projects as $k => $p) {
-		$tnums = count(@$p['tasks']);
+		$tnums = isset($p['tasks']) ? count($p['tasks']) : 0;
 		// don't show project if it has no tasks
 		// patch 2.12.04, show project if it is the only project in view
 		if ($tnums > 0 || $project_id == $p['project_id']) {
@@ -540,8 +540,8 @@ if ($showEditCheckbox) {
 			if (!$min_view) {
 				// not minimal view
 				?>
-				<form name="assFrm<?php echo ($p['project_id']) ?>" action="index.php?m=<?php echo ($m); ?>&amp;a=<?php echo ($a); ?>"
-					method="post">
+				<form name="assFrm<?php echo ($p['project_id']) ?>"
+					action="index.php?m=<?php echo ($m); ?>&amp;a=<?php echo ($a); ?>" method="post">
 					<input type="hidden" name="del" value="1" />
 					<input type="hidden" name="rm" value="0" />
 					<input type="hidden" name="store" value="0" />
@@ -637,7 +637,7 @@ if ($showEditCheckbox) {
 												}
 												?>
 											</select>
-										<?php
+											<?php
 										}
 										?>
 									</td>
@@ -792,7 +792,7 @@ if ($showEditCheckbox) {
    onclick="javascript:window.location='index.php?m=projects&a=reports&project_id=<?php echo $k; ?>';" />
 -->
 							<input type="button" class="button" value="<?php echo $AppUI->_('Gantt Chart'); ?>" onclick="javascript:window.location='index.php?m=tasks&amp;a=viewgantt&amp;project_id=<?php
-							  echo $k; ?>';" />
+							   echo $k; ?>';" />
 				</form>
 				</td>
 				</tr>
@@ -824,9 +824,9 @@ if ($showEditCheckbox) {
 		<td>=<?php echo $AppUI->_('Done'); ?>&nbsp;&nbsp;
 			<?php if ($min_view) { ?>
 				&nbsp;&nbsp;<a href="<?php echo 'index.php' . $query_string . '&amp;open_task_all=1'; ?>"><?php
-				   echo $AppUI->_('Expand All'); ?></a> :
+					   echo $AppUI->_('Expand All'); ?></a> :
 				<a href="<?php echo 'index.php' . $query_string . '&amp;close_task_all=1'; ?>"><?php
-				   echo $AppUI->_('Collapse All'); ?></a>
+					   echo $AppUI->_('Collapse All'); ?></a>
 			<?php }
 			?>
 		</td>
