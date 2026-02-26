9474018313434e6b00f078f82dafbcb0c5b1d776
David M. <david.montero.isusi@gmail.com>
2026-02-26 03:13:53 +0100

    Refactor: Replace `preg_match` with `parse_url` for base URL path extraction.

--- includes/session.php
+++ includes/session.php
@@ -225,8 +225,8 @@ function dpSessionStart($start_vars = 'AppUI')
 		$max_time = 0; // Browser session only.
 	}
 	// Try and get the correct path to the base URL.
-	preg_match('_^(https?://)([^/:]+)(:[0-9]+)?(/.*)?$_i', dPgetConfig('base_url'), $url_parts);
-	$cookie_dir = (isset($url_parts[4])) ? $url_parts[4] : '';
+	$url_info = parse_url(dPgetConfig('base_url', ''));
+	$cookie_dir = isset($url_info['path']) ? $url_info['path'] : '';
 	if (mb_substr($cookie_dir, 0, 1) != '/') {
 		$cookie_dir = '/' . $cookie_dir;
 	}
